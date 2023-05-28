4de9f1c85ce189bf821107f3d530da420b7a6798
iilocyi1 <124301687+iilocyi1@users.noreply.github.com>
2023-05-27 23:22:15 -0700

    Menu added

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public string sceneToLoad = "qsampol_FinalProjectB";
+
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
